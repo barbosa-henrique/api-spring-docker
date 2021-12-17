ce2997b8b08f1d5e176fbe78884cb090efcf9edd
Henrique Barbosa <henriquedouglasbarbosa@gmail.com>
2021-12-16 21:52:39 -0300

    Initial commit

---
+++ src/test/java/com/apispringdocker/ApiSpringDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apispringdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
